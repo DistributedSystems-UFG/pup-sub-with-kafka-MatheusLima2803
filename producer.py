b7216a29ec2219c2e2805ede5defddfa5eabdca6
MatheusLima2803 <matheuslima23@discente.ufg.br>
2025-11-09 22:05:31 -0300

    Update producer.py

--- producer.py
+++ producer.py
@@ -2,16 +2,27 @@ from kafka import KafkaProducer
 from const import *
 import sys
 
-try:
-    topic = sys.argv[1]
-except:
-    print ('Usage: python3 producer <topic_name>')
+if len(sys.argv) < 3:
+    print("Uso: python3 produtor_grupo.py <grupo> <seu_nome>")
     exit(1)
-    
-producer = KafkaProducer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
-for i in range(100):
-    msg = 'My ' + str(i) + 'st message for topic ' + topic
-    print ('Sending message: ' + msg)
-    producer.send(topic, value=msg.encode())
 
-producer.flush()
+topico = sys.argv[1]
+user = sys.argv[2]  # nome do usuario no chat
+
+# Criar produtor Kafka
+produtor = KafkaProducer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
+
+print(f"Entrou no chat **{topico}** como {user} 😎")
+print("Digite sua mensagem e ENTER para mandar (digite /sair pra sair)\n")
+
+while True:
+    txt = input(f"{user} >>> ")
+    
+    if txt == "/sair":
+        print("Saiu")
+        break
+    
+    msg = f"{user}: {txt}"
+  
+    produtor.send(topico, value=msg.encode())
+    produtor.flush()
